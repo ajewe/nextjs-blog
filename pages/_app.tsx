8d34447475e344333a568914e0ed0af9c2751624
ajewe <ajewe@utexas.edu>
2020-11-29 13:18:57 -0600

    converted files to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/global.css";
+import { AppProps } from "next/app";
 //can import global CSS file from here AND ONLY HERE, doesn't work anywhere else
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
